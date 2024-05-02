3c4e736d210c41fcd0ad905a71b69c67724577d5
Brendan Barrett <bbarrett@ab.mpg.de>
2024-05-02 16:28:37 +0200

    some tiny cahnges

--- code/02_code_glmm_prep.R
+++ code/02_code_glmm_prep.R
@@ -1,4 +1,6 @@
 library(lubridate)
+library(rethinking)
+set_ulam_cmdstan(TRUE) #set cmdstan to true
 # collate dataframe
 # number of tool use events in a cell (22m or 110m) per individual per day
 # hours spent following an indivdual ona a given day
@@ -11,11 +13,16 @@ str(grid_22m)
 str(focals_data_cropped)
 focals_data_cropped$date <- date(focals_data_cropped$timestamp) #unique date
 
-temp <- aggregate(nutcrackin ~ individual + date + corresponding_cell_id , focals_data_cropped, sum) # number of nutcracks per individual per day across all of data
+# number of nutcracks per individual per day across all of data
+daily_crack <- aggregate(nutcrackin ~ individual + date + corresponding_cell_id , focals_data_cropped, sum) 
 hist(focals_data_cropped$nutcrackin)
-hist(temp$nutcrackin)
+hist(daily_crack$nutcrackin)
+str(daily_crack)
+unique(daily_crack$individual)
+unique(focals_data_cropped$individual)
 str(focals_data_cropped)
 str(heights_data) ## this is where i will get sampling time by summing up minutes per day
+
 #need unique number of minutes per follow 
 heights_data$date <- date(heights_data$timestamp) #create dates
 temp <- aggregate(minute ~ individual + date + focal, heights_data, length) # number of nutcracks per individual across all of data
@@ -29,13 +36,64 @@ hist(temp$minutes_followed) #there is a duplicated follow, check up on it
 #WERE MONKEYS IN A CELL, AND IF THEY WERE DOES WAS NUT CRACKING OBSERVED THERE, IRRESPECTIVE OF TIME
 # HOW MUCH TIME TO MONKEYS SPEND IN CELLS
 # GIVEN A MONKEY WAS IN A CELL FOR X TIME, WHAT WAS THEIR DAILY RATE OF TOOL USE THEIR
+
 # intercepts only poisson
-m0 <- ulam(
+data_list_daily <- list(
+  nutcrackin = daily_crack$nutcrackin
+)
+##ZIP intercepts only of 1/0
+m0ZIP <- ulam(
   alist(
-    y ~ dzipois( p , lambda ),
+    nutcrackin ~ dzipois( p , lambda ),
     logit(p) <- ap,
     log(lambda) <- al,
-    ap ~ dnorm( -1.5 , 1 ),
-    al ~ dnorm( 1 , 0.5 )
-  ) , data=list(y=y) , chains=4 )
-precis( m12.3 )
+    ap ~ dnorm( 0 , 1 ),
+    al ~ dnorm( 0 , 2 )
+  ) , data=data_list_daily , chains=4)
+
+precis( m0ZIP )
+mean(daily_crack$nutcrackin)
+exp(-0.24)
+
+post <- extract.samples(m0ZIP)
+
+## bernouli count of mean
+# craete a list
+data_list <- list(
+  nutcrackin = focals_data_cropped$nutcrackin
+)
+
+rpois(n=1000 , lambda=2 )
+simplehist(rpois(n=10000 , lambda=mean(exp(post$al)) ) )
+m0 <- ulam(
+  alist(
+    nutcrackin ~ binomial(1,p),
+    logit(p) <- ap ,
+    ap ~ dnorm( 0 , 1 )
+    ) , data=data_list , chains=4)
+
+# to go from probability to log-odds use logit link 
+logit(.99)
+# to go from log-odds to proability/real scale use
+logistic(.99)
+inv_logit(.99)
+
+post <- extract.samples(m0)
+str(post)
+dens(post$ap) #log odds scale
+dens(logistic(post$ap)) #log odds scale
+abline(v=mean(logistic(post$ap)))
+precis( m0 ) # real probability scale
+inv_logit(-1.27)
+mean(focals_data_cropped$nutcrackin)
+
+
+###what cuses terrestriality
+str(grid_22m)
+str(focals_data_cropped)
+focals_data_cropped$corresponding_cell_id
+grid_22m$cell_id
+
+joined_df <- merge(focals_data_cropped,grid_22m, by.x = "corresponding_cell_id", 
+                  by.y = "cell_id", all.x = TRUE, all.y = FALSE)
+
